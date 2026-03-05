121ce0839f8a17beda523ea7f3ddfb2355068728
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-03-05 13:03:11 -0300

    feat
    brincando um pouco com os estilos do react drawer navigator

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './src/screens/Home';
 import Test from './src/screens/Test';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
 
@@ -9,9 +10,37 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Início' component={Home} />
-        <Drawer.Screen name='Teste' component={Test} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#8F5ED6"
+          },
+          headerTintColor: "#000000",
+          headerTitleStyle: {
+            fontWeight: "regular"
+          },
+          drawerStyle: {
+            backgroundColor: '#774eb0',
+            width: 275,
+          },
+          overlayColor: "#905ed62a",
+          drawerActiveBackgroundColor: "#5e3d8c",
+          drawerActiveTintColor: "white"
+        }}
+      >
+        <Drawer.Screen 
+          name='Início' 
+          component={Home} 
+          options={{
+            drawerIcon: ({ size, color }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen 
+          name='Teste' 
+          component={Test} 
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
